20a1006820ca443c7493c0ef5bccef337db57fd3
Him188 <1040400290@qq.com>
2017-05-03 13:59:19 +0800

    Removed useless code

--- src/main/java/net/mamoe/moedb/MoeDB.java
+++ src/main/java/net/mamoe/moedb/MoeDB.java
@@ -5,10 +5,7 @@ import cn.nukkit.plugin.PluginBase;
 /**
  * @author Him188 @ MoeDB Project
  * @since MoeDB 1.0.0
-*/
+ */
 public class MoeDB extends PluginBase {
-	@Override
-	public void onLoad() {
-		super.onLoad();
-	}
+
 }
